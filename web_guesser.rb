ec7db1f5b1e8ef8c76550a39a0387670a796823d
creaked <creaked@gmail.com>
2017-10-11 05:30:05 +0000

    guess checking operational

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+number = rand(100).to_i
+@color_var = '#fff'
 
-get '/test' do
-  erb :index, :locals => {:number => number}
+def guess(guess, n)
+  #case guess
+  if guess == 0
+    guess = " "
+  elsif guess > n
+    @color_var = '#f00' # red
+    guess = "Too high!"
+  elsif guess < n
+    @color_var = '#ff0' # yellow
+    guess = "Too low!"
+  elsif guess == n
+    @color_var = '#0f0' # green
+    guess = "Correct! The secret number is #{n}!"
+  end
+end 
+
+get '/' do
+ guess = params["guess"]
+ check_guess = guess(guess.to_i, number)
+ erb :index, :locals => {:number => number, :guess => guess, :check_guess => check_guess, :color_var => @color_var}
 end
